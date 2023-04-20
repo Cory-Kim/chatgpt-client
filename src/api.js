761d98dfc78697a61bff0960b898451ad4541f55
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-19 22:25:48 -0700

    changed one problem

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse = async (chat) =>
 {
   try {
-    const response = await fetch('https://chatgpt-server-pink.vercel.app/', {
+    const response = await fetch('https://vercel.com/cory-kim/chatgpt-server', {  
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
