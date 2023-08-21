037b87df86928cd217ef70930ab08f048d9b31aa
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-08-21 01:35:00 -0700

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { QueryClientProvider, QueryClient } from 'react-query'
-import './index.css'
+// main.jsx
 
-const queryClient = new QueryClient();
+// Import any necessary modules here
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const userInput = document.getElementById('userInput').value;
+
+    try {
+        const response = await fetch('https://chatgpt-server-dv4rimw58-cory-kim.vercel.app/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: userInput }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            displayResponse(data.message); // Assuming displayResponse is a function that displays the response on the webpage
+        } else {
+            console.error('Request failed with status:', response.status);
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+// Function to display the response on the webpage
+function displayResponse(message) {
+    const responseElement = document.createElement('p');
+    responseElement.textContent = `Response: ${message}`;
+    rootElement.appendChild(responseElement);
+}
+
+// Attach event listener to form submission
+const form = document.getElementById('userForm');
+form.addEventListener('submit', handleSubmit);
+
+// Rest of the code...
